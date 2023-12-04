7e17615724769b0bd077151369817dcfef0830b3
moyasser02 <mohamedyasserktb20@gmail.com>
2023-12-04 16:06:17 +0200

    Inst queue and reg file (1)

---
+++ node_modules/InstructionsEnum.js
@@ -0,0 +1,13 @@
+const instructions = {
+    SUB: 'SUB',
+    ADD: 'ADD',
+    ADDI: 'ADDI',
+    SUBI: 'SUBI',
+    MUL: 'MUL',
+    DIV: 'DIV',
+    BNEZ: 'BNEZ',
+    LD:"L.D",
+    SD:"S.D"
+  };
+  
+module.exports= instructions;
